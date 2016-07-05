9ce069665e414a468fc82c4e754bd4b5c33fd635
Felipe Antunes <felipeapb@gmail.com>
2016-07-05 16:29:58 -0300

    alteracao as 16:30

--- js/init.js
+++ js/init.js
@@ -51,30 +51,30 @@ $(function(){ //
       }
   });
 var fundo= 2
-$('.texto1').fadeIn(500);
+$('.texto1').fadeIn(200);
 var texto=1;
 setInterval(function(){
  if(fundo==7){ fundo=1;}
   if(texto==6){ texto=0;}
 texto++;
 
-$('.alternado').removeClass('alt1').fadeOut(500);
-$('.alternado').removeClass('alt2').fadeOut(500);
-$('.alternado').removeClass('alt3').fadeOut(500);
-$('.alternado').removeClass('alt4').fadeOut(500);
-$('.alternado').removeClass('alt5').fadeOut(500);
-$('.alternado').removeClass('alt6').fadeOut(500);
-$('.texto1').fadeOut(500);
-$('.texto2').fadeOut(500);
-$('.texto3').fadeOut(500);
-$('.texto4').fadeOut(500);
-$('.texto5').fadeOut(500);
-$('.texto6').fadeOut(500);
+$('.alternado').removeClass('alt1').fadeOut(200);
+$('.alternado').removeClass('alt2').fadeOut(200);
+$('.alternado').removeClass('alt3').fadeOut(200);
+$('.alternado').removeClass('alt4').fadeOut(200);
+$('.alternado').removeClass('alt5').fadeOut(200);
+$('.alternado').removeClass('alt6').fadeOut(200);
+$('.texto1').fadeOut(200);
+$('.texto2').fadeOut(200);
+$('.texto3').fadeOut(200);
+$('.texto4').fadeOut(200);
+$('.texto5').fadeOut(200);
+$('.texto6').fadeOut(200);
 $('.bl').removeClass('seletoresverde');
 $('.bol'+fundo).addClass('seletoresverde');
 
-$('.alternado').addClass('alt'+fundo++).fadeIn(1050);
-$('.texto'+texto).fadeIn(1050);
+$('.alternado').addClass('alt'+fundo++).fadeIn(300);
+$('.texto'+texto).fadeIn(300);
 
 
  }, 4000);
